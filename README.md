891c8097b0c560deb4fdba7a27072fac3f4190ad
João Matos <joao.deus@usp.br>
2024-06-13 19:10:32 -0300

    login funcionando com integracao com o banco

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Ir ate o diretorio login-page
 ### `npm install`
 
 ### `npm start`
+
+Em outro terminal ir ate o diretorio backend
+
+### `python .\main.py`
+
+Login valido para tentativas
+
+user 123.456.789-00
+senha 123
---
+++ backend/main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import oracledb
+
+app = Flask(__name__)
+CORS(app)  # Habilita CORS para todos os endpoints
+
+# Informações de conexão
+un = 'a12677492'
+pw = 'a12677492'
+host = 'orclgrad1.icmc.usp.br'
+port = 1521
+service_name = 'pdb_elaine.icmc.usp.br'
+
+dsn = f"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={host})(PORT={port}))(CONNECT_DATA=(SERVICE_NAME={service_name})))"
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    try:
+        with oracledb.connect(user=un, password=pw, dsn=dsn) as connection:
+            with connection.cursor() as cursor:
+                sql = "SELECT Userid FROM USERS WHERE IdLider = :username AND Password = :password"
+                cursor.execute(sql, [username, password])
+                result = cursor.fetchone()
+                if result:
+                    return jsonify({"message": "Login successful", "userid": result[0]}), 200
+                else:
+                    return jsonify({"message": "Invalid credentials"}), 401
+    except Exception as e:
+        return jsonify({"message": f"An error occurred: {e}"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
