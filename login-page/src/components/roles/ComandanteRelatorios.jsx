a8b57d76b808e2455a188de3d3266a1830bdf7c2
Lucas Mihara <miharalucas@gmail.com>
2024-06-16 22:03:52 -0300

    relatorio comandante

--- login-page/src/components/roles/ComandanteRelatorios.jsx
+++ login-page/src/components/roles/ComandanteRelatorios.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Container, Table } from "reactstrap";
 
 const Relatorios = () => {
+  const [planetas, setPlanetas] = useState([]);
+
   const exibirRelatorio = async (tipo) => {
     console.log(tipo)
     try {
-      const response = await axios.post(`http://localhost:5000/api/relatorio/comandante/informacoes_estrategicas`);
-      console.log(response.data); // Aqui você pode fazer algo com os dados do relatório, como atualizar o estado no componente React
+      const response = await axios.post(`http://localhost:5000/api/relatorio/comandante/planetas_em_potencial`);
+      setPlanetas(response.data.dados);
     } catch (error) {
       console.error('Erro ao obter relatório:', error);
     }
@@ -16,8 +19,28 @@ const Relatorios = () => {
     <div>
       <h2>Relatórios</h2>
       <button onClick={() => exibirRelatorio('estrela')}>Relatório de Estrelas</button>
-      <button onClick={() => exibirRelatorio('planeta')}>Relatório de Planetas</button>
-      <button onClick={() => exibirRelatorio('sistema')}>Relatório de Sistemas</button>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Id Astronomico</th>
+            <th>Massa</th>
+            <th>Raio</th>
+            <th>Classificacao</th>
+            <th>Sistema</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planetas.map((planeta, index) => (
+            <tr key={index}>
+              <td>{planeta[0]}</td>
+              <td>{planeta[1]}</td>
+              <td>{planeta[2]}</td>
+              <td>{planeta[3]}</td>
+              <td>{planeta[4]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
